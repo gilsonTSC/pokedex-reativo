555f1b8af4bc1dd68dd0c3fd04fd44e25ceb610f
Gilson <gilson.costa@dce.ufpb.br>
2020-08-06 20:50:00 -0300

    Adicionando o delete e update no controller

--- src/main/java/com/gilsontsc/pokedex/controller/PokemonController.java
+++ src/main/java/com/gilsontsc/pokedex/controller/PokemonController.java
@@ -2,9 +2,11 @@ package com.gilsontsc.pokedex.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -45,4 +47,34 @@ public class PokemonController {
 		return this.repo.save(pokemon);
 	}
 	
+	@PutMapping("{id}")
+    public Mono<ResponseEntity<Pokemon>> updatePokemon(@PathVariable(value = "id") String id,
+                                                       @RequestBody Pokemon pokemon) {
+        return this.repo.findById(id)
+                .flatMap(existingPokemon -> {
+                    existingPokemon.setNome(pokemon.getNome());
+                    existingPokemon.setCategoria(pokemon.getCategoria());
+                    existingPokemon.setHabilidade(pokemon.getHabilidade());
+                    existingPokemon.setPeso(pokemon.getPeso());
+                    return this.repo.save(existingPokemon);
+                })
+                .map(updatePokemon -> ResponseEntity.ok(updatePokemon))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+
+    }
+	
+    @DeleteMapping("{id}")
+    public Mono<ResponseEntity<Void>> deletePokemon(@PathVariable(value = "id") String id) {
+        return this.repo.findById(id)
+                .flatMap(existingPokemon ->
+                        this.repo.delete(existingPokemon)
+                                 .then(Mono.just(ResponseEntity.ok().<Void>build()))
+                )
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+    
+    @DeleteMapping
+    public Mono<Void> deleteAllPokemons() {
+        return this.repo.deleteAll();
+    }
 }
